399b24d927ef4dba22396d709b27b92b7427df2e
Julio Pelucio <juliopeluciofox@hotmail.com>
2021-10-19 23:37:39 -0300

    feat(lance): creating a test case for when a value is valid on 'lance'.

--- src/tests/unit/Lance.spec.js
+++ src/tests/unit/Lance.spec.js
@@ -3,5 +3,28 @@ import { mount } from '@vue/test-utils'
 
 test('não aceita lance com valor menor do que zero', () => {
     const wrapper = mount(Lance)
-    expect(wrapper).toBeTruthy()
+    const input = wrapper.find('input')
+    input.setValue(-100)
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    wrapper.trigger('submit')
+    expect(lancesEmitidos).toBeUndefined()
+})
+
+test('emite um lance quando o valor é maior do que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+})
+
+test('emite o valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const lance = parseInt(lancesEmitidos[0][0])
+    expect(lance).toBe(100)
 })
