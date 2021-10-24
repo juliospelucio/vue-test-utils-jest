da6051555f90282466d7dd648641bf28a9dbf2a5
Julio Pelucio <juliopeluciofox@hotmail.com>
2021-10-24 18:45:15 -0300

    feat(lance): Defining a min value for a lance.

--- src/tests/unit/Lance.spec.js
+++ src/tests/unit/Lance.spec.js
@@ -28,3 +28,34 @@ test('emite o valor esperado de um lance válido', () => {
     const lance = parseInt(lancesEmitidos[0][0])
     expect(lance).toBe(100)
 })
+
+describe('um lance com valor minimo', () => {
+    test('todos os lances devem possuir um valor maior do que o minimo informado', () => {
+        const wrapper = mount(Lance, {
+            propsData: {
+                lanceMinimo: 300
+            }
+        })
+
+        const input = wrapper.find('input')
+        input.setValue(400)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        expect(lancesEmitidos).toHaveLength(1)
+    })
+
+    test('emite o valor esperado de um lance valido', () => {
+        const wrapper = mount(Lance, {
+            propsData: {
+                lanceMinimo: 300
+            }
+        })
+
+        const input = wrapper.find('input')
+        input.setValue(400)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        const valorDoLance = parseInt(lancesEmitidos[0][0])
+        expect(valorDoLance).toBe(400)
+    })
+})
